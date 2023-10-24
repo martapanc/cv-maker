a114ed3aafbce083b4097f49e0a8be3ef9e47eba
marta.pancaldi <marta.pancaldi@resourcify.de>
2023-10-24 20:04:40 +0200

    feat: skills on a column

--- src/components/cv/cards/WorkCard.tsx
+++ src/components/cv/cards/WorkCard.tsx
@@ -9,9 +9,19 @@ export interface WorkCardProps {
 const WorkCard: React.FC<WorkCardProps> = ({ job }) => {
   return (
     <div className='mb-1 flex'>
-      <div className='my-2 flex w-[29.5%] flex-col items-end border-e-[1.5px] border-slate-300 pe-3'>
-        <span className='mt-1 text-sm font-light'>{job.dates}</span>
-        <span className='text-[15.8px]'>{job.jobTitle}</span>
+      <div className='my-2 flex w-[29.5%] flex-col items-end justify-between border-e-[1.5px] border-slate-300 pe-3'>
+        <div className='flex flex-col items-end'>
+          <span className='mt-1 text-sm font-light'>{job.dates}</span>
+          <span className='text-[15.8px]'>{job.jobTitle}</span>
+        </div>
+
+        <div className='flex flex-col items-end'>
+          {job.skills.map((skill, id) => (
+            <span key={id} className='text-xs'>
+              {skill}
+            </span>
+          ))}
+        </div>
       </div>
       <div className='my-2 flex w-[70.5%] flex-col ps-4'>
         <span className='text-lg font-semibold'>{job.name}</span>
