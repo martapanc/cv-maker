4b22c52618f61ce6526538557a5c71fdc887964a
martapanc <marta.panc@gmail.com>
2024-02-23 19:18:38 +0100

    feat: add dev resources and more examples; pass url as CL arg

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # CV Maker
+
+## Resources
+
+- https://pptr.dev/
+- https://blog.risingstack.com/pdf-from-html-node-js-puppeteer/
+- https://codeburst.io/high-quality-pdf-generation-using-puppeteer-5b51e2ab231a
+- https://blog.theodo.com/2021/10/pdf-generation-react-puppeteer/
--- generatePDF.js
+++ generatePDF.js
@@ -2,14 +2,18 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
+  const url = process.argv[2] || 'https://martacodes.it/cv'; // Get the URL from command-line argument
+
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.setViewport({ width: 1440, height: 900, deviceScaleFactor: 2 });
-  await page.goto('http://localhost:3004/puppeteer', {
+
+  await page.goto(url, {
     waitUntil: 'networkidle2',
   });
+
   await page.pdf({
-    path: 'output/resume.pdf',
+    path: `output/${new Date().toISOString()}_output.pdf`,
     pageRanges: '1-2',
     format: 'A4',
     printBackground: true,
